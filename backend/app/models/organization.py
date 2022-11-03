30070577a01dc4a64dc10fb0f492da7d7ab3319d
Chris Dare <engineering@chrisdare.me>
2022-11-03 17:58:43 +0000

    Delete encode models
    Will be using sqlmodel instead of encode's orm

--- backend/app/models/organization.py
+++ backend/app/models/organization.py
@@ -3,16 +3,16 @@ data concerning organizations on HyperSenta
 """
 # Author: Christopher Dare
 
+from typing import Any, Dict, Optional
+
+from pydantic import EmailStr, validator
+from sqlmodel import Field, Column
+import sqlalchemy as sa
+
+from .abstract import TimeStampedModel
+
+
 from app.config.api_config import api_settings
 import orm
 
 
-class Organization(orm.Model):
-    tablename = "organization"
-    registry = api_settings.DEFAULT_MODEL_REGISTRY
-    fields = {
-        "id": orm.Integer(primary_key=True),
-        "name": orm.String(max_length=100),
-        "owner": orm.String(max_length=100), # TODO
-        "is_active": orm.Boolean(default=False),
-    }
