3a4757e653e071062bb35d45db4cde73c6273886
Chris Dare <engineering@chrisdare.me>
2022-11-03 18:08:19 +0000

    Disable removal of unused imports

--- backend/scripts/format.sh
+++ backend/scripts/format.sh
@@ -1,6 +1,7 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py
+# autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py --exclude=migrations/
+autoflake --recursive --remove-unused-variables --in-place app --exclude=__init__.py --exclude=migrations/
 black app
 isort --recursive --apply app
