a471136e849b6a3eceb6b2fa22937777a8690050
Chris Dare <engineering@chrisdare.me>
2022-11-13 00:31:19 +0000

    Implement user login
    Doesn't stop inactive users from logging in.

--- backend/app/api/api_v1/endpoints/login.py
+++ backend/app/api/api_v1/endpoints/login.py
@@ -1,11 +1,15 @@
+from datetime import timedelta
 from typing import Any
 
-from fastapi import APIRouter, Body, Depends
+from fastapi import APIRouter, Body, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from app import models, schemas
+from app import crud, models, schemas
 from app.api import deps
+from app.core import security
+from app.core.config import settings
+
 
 router = APIRouter()
 
@@ -17,7 +21,20 @@ def login_access_token(
     """
     OAuth2 compatible token login, get an access token for future requests
     """
-    raise NotImplementedError
+    user = crud.user.authenticate(
+        db, email=form_data.username, password=form_data.password
+    )
+    if not user:
+        raise HTTPException(status_code=400, detail="Incorrect email or password")
+    # elif not crud.user.is_active(user):
+    #     raise HTTPException(status_code=400, detail="Inactive user")
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+    return {
+        "access_token": security.create_access_token(
+            user.uuid, expires_delta=access_token_expires
+        ),
+        "token_type": "bearer",
+    }
 
 
 @router.post("/login/test-token", response_model=schemas.User)
