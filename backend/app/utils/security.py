56e2e773f7c064b418592e5d6ba0cacbc30a39ac
Chris Dare <engineering@chrisdare.me>
2022-11-20 19:54:12 +0000

    Add password reset utilities

--- backend/app/utils/security.py
+++ backend/app/utils/security.py
@@ -3,7 +3,10 @@
 # Author: Christopher Dare
 
 import hashlib
+from typing import Optional
+from jose import jwt
 
+from app.core.config import settings
 
 def make_password(raw_password: str) -> str:
     assert raw_password
@@ -15,3 +18,23 @@ def check_password(hash: str, raw_password: str) -> bool:
     """Generates the hash for a raw_password and compares it."""
     generated_hash = make_password(raw_password)
     return hash == generated_hash
+
+
+def generate_password_reset_token(email: str) -> str:
+    delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
+    now = datetime.utcnow()
+    expires = now + delta
+    exp = expires.timestamp()
+    encoded_jwt = jwt.encode(
+        {"exp": exp, "nbf": now, "sub": email}, settings.SECRET_KEY, algorithm="HS256",
+    )
+    return encoded_jwt
+
+
+def verify_password_reset_token(token: str) -> Optional[str]:
+    try:
+        decoded_token = jwt.decode(token, settings.SECRET_KEY, algorithms=["HS256"])
+        return decoded_token["email"]
+    except jwt.JWTError:
+        return None
+
