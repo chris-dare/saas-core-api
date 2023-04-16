2aa975a0916e3667b45f3faa12b6e3b6a53e4ba8
Chris Dare <engineering@chrisdare.me>
2023-04-16 09:40:47 +0000

    Delete legacy user schema

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Session
 
-from app import crud, schemas
+from app import crud, models
 from app.core.config import settings
 from app.db import base  # noqa: F401
 
@@ -17,9 +17,10 @@ def init_db(db: Session) -> None:
 
     user = crud.user.get_by_email(db, email=settings.FIRST_SUPERUSER)
     if not user:
-        user_in = schemas.UserCreate(
+        user_in = models.UserCreate(
+            first_name="Hypersenta",
+            last_name="Admin",
             email=settings.FIRST_SUPERUSER,
-            password=settings.FIRST_SUPERUSER_PASSWORD,
-            is_superuser=True,
+            password=settings.FIRST_SUPERUSER_PASSWORD
         )
-        user = crud.user.create(db, obj_in=user_in)  # noqa: F841
+        user = crud.user.create(db, obj_in=user_in, is_superuser=True)  # noqa: F841
