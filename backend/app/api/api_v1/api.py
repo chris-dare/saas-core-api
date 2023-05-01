40f0b225d66110d3e9cd752f2b4c62e674260d08
Chris Dare <engineering@chrisdare.me>
2023-05-01 22:34:35 +0000

    Unpublish org and org member APIs

--- backend/app/api/api_v1/api.py
+++ backend/app/api/api_v1/api.py
@@ -9,8 +9,8 @@ api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 # important to list organization members before organization
 # due to how the endpoints are registered by the framework
 # Do not change unless you really know what you are doing
-api_router.include_router(organization_members.router, prefix="/organizations", tags=["organization_members"])
-api_router.include_router(organizations.router, prefix="/organizations", tags=["organizations"])
+# api_router.include_router(organization_members.router, prefix="/organizations", tags=["organization_members"])
+# api_router.include_router(organizations.router, prefix="/organizations", tags=["organizations"])
 api_router.include_router(events.router, prefix="/events", tags=["events"])
 api_router.include_router(bills.router, prefix="/bills", tags=["bills"])
 api_router.include_router(transactions.router, prefix="/transactions", tags=["transactions"])
