679fb454a96d1b9c125bf9a90294c0d85763bf0f
Chris Dare <engineering@chrisdare.me>
2023-11-15 15:23:20 +0000

    Add test for user creation

--- backend/tests/test_users.py
+++ backend/tests/test_users.py
@@ -41,7 +41,7 @@ def get_testing_app(override_db: bool = True,):
         app.dependency_overrides[get_session] = get_session_override
     return app
 
-def test_create_patient_user():
+def test_onboard_user():
     # Sign up a user with only their first and last name, plus their mobile number
     client = TestClient(get_testing_app())
     user_create_endpoint = "/v1/users/sign-up"
@@ -56,8 +56,24 @@ def test_create_patient_user():
         user_create_endpoint, json=payload
     )
 
-    app.dependency_overrides.clear()
-
     logging.info(f"Create user response: {response.json()}")
 
+    # test user sign up with duplicate mobile number
+
     assert response.status_code == 200
+
+    payload = {
+        "first_name": "Test",
+        "last_name": "User",
+        "mobile": "+2348030000000",
+    }
+
+    response = client.post(
+        user_create_endpoint, json=payload
+    )
+
+    logging.info(f"Create duplicate user response: {response.json()}")
+
+    app.dependency_overrides.clear()
+
+    assert response.status_code == 400
